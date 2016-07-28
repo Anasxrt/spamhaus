4dd3c298494df625948b19d4d0314651188e26c0
Anasxrt <Anasxrt@users.noreply.github.com>
2016-07-28 23:08:41 +0700

    Update and rename spamhaus.sh to DROP_CR.sh

--- DROP_CR.sh
+++ DROP_CR.sh
@@ -4,17 +4,26 @@
 # http://www.theunsupported.com/2012/07/block-malicious-ip-addresses/
 # http://www.cyberciti.biz/tips/block-spamming-scanning-with-iptables.html
 
+#
+# Emerging Threats fwip rules.
+#
+# Raw IPs for the firewall block lists. These come from:
+#
+# Spam nets identified by Spamhaus (www.spamhaus.org)
+# Top Attackers listed by DShield (www.dshield.org)
+# Abuse.ch
+
 # path to iptables
 IPTABLES="/sbin/iptables";
 
 # list of known spammers
-URL="www.spamhaus.org/drop/drop.lasso";
+URL="https://rules.emergingthreats.net/fwrules/emerging-Block-IPs.txt";
 
 # save local copy here
-FILE="/tmp/drop.lasso";
+FILE="/tmp/emerging-Block-IPs.txt";
 
 # iptables custom chain
-CHAIN="Spamhaus";
+CHAIN="CloudRambo";
 
 # check to see if the chain already exists
 $IPTABLES -L $CHAIN -n
@@ -25,7 +34,7 @@ if [ $? -eq 0 ]; then
     # flush the old rules
     $IPTABLES -F $CHAIN
 
-    echo "Flushed old rules. Applying updated Spamhaus list...."    
+    echo "Flushed old rules. Applying updated DROP list...."    
 
 else
 
@@ -38,7 +47,7 @@ else
     # don't allow this traffic through
     $IPTABLES -A FORWARD -j $CHAIN
 
-    echo "Chain not detected. Creating new chain and adding Spamhaus list...."
+    echo "Chain not detected. Creating new chain and adding DROP list...."
 
 fi;
 
@@ -46,10 +55,10 @@ fi;
 wget -qc $URL -O $FILE
 
 # iterate through all known spamming hosts
-for IP in $( cat $FILE | egrep -v '^;' | awk '{ print $1}' ); do
+for IP in $( cat $FILE | egrep -v '(^;|^#.*|^$)' | awk '{ print $1}' ); do
 
     # add the ip address log rule to the chain
-    $IPTABLES -A $CHAIN -p 0 -s $IP -j LOG --log-prefix "[SPAMHAUS BLOCK]" -m limit --limit 3/min --limit-burst 10
+    $IPTABLES -A $CHAIN -p 0 -s $IP -j LOG --log-prefix "[CloudRambo DROP]" -m limit --limit 3/min --limit-burst 10
 
     # add the ip address to the chain
     $IPTABLES -A $CHAIN -p 0 -s $IP -j DROP
